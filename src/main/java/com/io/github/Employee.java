f3d38f07177e63b7840aa6e2079447e182c17b8b
Revathi G <revathikrishna0323@gmail.com>
2022-10-13 20:22:22 +0530

    created a constructor and CalculateSalary() method

--- src/main/java/com/io/github/Employee.java
+++ src/main/java/com/io/github/Employee.java
@@ -11,5 +11,16 @@ public class Employee {
     private float salary;
     private float bonus;
 
+    public Employee(String empName, float salary, float bonus) {
+        this.empName = empName;
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+
+    public float calculateSalary() {
+        float totalSalary = salary + bonus;
+        return totalSalary;
+
+    }
 
 }
